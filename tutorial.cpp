4a2060595c10024a19a6c527a37719bfe4aa0748
zjeffer <vanhouttetuur@gmail.com>
2020-12-21 18:06:22 +0100

    lesson 8: geometry rendering

--- tutorial.cpp
+++ tutorial.cpp
@@ -74,13 +74,7 @@ bool loadMedia() {
     //Loading success flag
     bool success = true;
 
-    //Load PNG surface
-    gTexture = loadTexture("img/texture.png");
-    if (gTexture == NULL) {
-        printf("Failed to load texture image!\n");
-        success = false;
-    }
-
+	// nothing to load
     return success;
 }
 
@@ -146,11 +140,29 @@ int main(int argc, char* args[]) {
                         quit = true;
                     }
                 }
-				// cllear screen
+				// clear screen
+				SDL_SetRenderDrawColor(gRenderer, 0xff, 0xff, 0xff, 0xff);
 				SDL_RenderClear(gRenderer);
 
-				// render texture to screen
-				SDL_RenderCopy(gRenderer, gTexture, NULL, NULL);
+				// render red filled quad
+				SDL_Rect fillRect = { SCREEN_WIDTH / 4, SCREEN_HEIGHT / 4, SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2};
+				SDL_SetRenderDrawColor(gRenderer, 0xff, 0x00, 0x00, 0xff);
+				SDL_RenderFillRect(gRenderer, &fillRect);
+
+				// render green outlined quad
+				SDL_Rect outlineRect = {SCREEN_WIDTH / 6, SCREEN_HEIGHT / 6, SCREEN_WIDTH * 2 / 3, SCREEN_HEIGHT * 2 / 3};
+				SDL_SetRenderDrawColor(gRenderer, 0x00, 0xff, 0x00, 0xff);
+				SDL_RenderDrawRect(gRenderer, &outlineRect);
+
+				// draw blue horizontal line
+				SDL_SetRenderDrawColor(gRenderer, 0x00, 0x00, 0xff, 0xff);
+				SDL_RenderDrawLine(gRenderer, 0, SCREEN_HEIGHT / 2, SCREEN_WIDTH, SCREEN_HEIGHT / 2);
+
+				// draw vertical line of yellow dots
+				SDL_SetRenderDrawColor(gRenderer, 0xff, 0xff, 0x00, 0xff);
+				for(int i = 0; i < SCREEN_HEIGHT; i+=4){
+					SDL_RenderDrawPoint(gRenderer, SCREEN_WIDTH / 2, i);
+				}
 
 				// update screen
 				SDL_RenderPresent(gRenderer);
