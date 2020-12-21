500f0188a0b960154d3a8d051869751e98e9d107
zjeffer <vanhouttetuur@gmail.com>
2020-12-21 18:45:25 +0100

    lesson 9: the viewport

--- tutorial.cpp
+++ tutorial.cpp
@@ -5,6 +5,7 @@ and may not be redistributed without written permission.*/
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
 #include <stdio.h>
+
 #include <string>
 
 //Screen dimension constants
@@ -32,7 +33,6 @@ SDL_Renderer* gRenderer = NULL;
 //Current displayed texture
 SDL_Texture* gTexture = NULL;
 
-
 bool init() {
     //Initialization flag
     bool success = true;
@@ -49,21 +49,21 @@ bool init() {
             success = false;
         } else {
             // create renderer for windows
-			gRenderer = SDL_CreateRenderer(gWindow, -1, SDL_RENDERER_ACCELERATED);
-			if(gRenderer == NULL){
-            	printf("Renderer could not be created! SDL Error: %s\n", SDL_GetError());
-				success = false;
-			} else {
-				// initialize renderer color
-				SDL_SetRenderDrawColor(gRenderer, 0xFF, 0xFF, 0xFF, 0xFF);
+            gRenderer = SDL_CreateRenderer(gWindow, -1, SDL_RENDERER_ACCELERATED);
+            if (gRenderer == NULL) {
+                printf("Renderer could not be created! SDL Error: %s\n", SDL_GetError());
+                success = false;
+            } else {
+                // initialize renderer color
+                SDL_SetRenderDrawColor(gRenderer, 0xFF, 0xFF, 0xFF, 0xFF);
 
-				// initialize png loading
-				int imgFlags = IMG_INIT_PNG;
-				if(!(IMG_Init(imgFlags) & imgFlags)){
-					printf( "SDL_image could not initialize! SDL_image Error: %s\n", IMG_GetError());
-					success = false;
-				}
-			}
+                // initialize png loading
+                int imgFlags = IMG_INIT_PNG;
+                if (!(IMG_Init(imgFlags) & imgFlags)) {
+                    printf("SDL_image could not initialize! SDL_image Error: %s\n", IMG_GetError());
+                    success = false;
+                }
+            }
         }
     }
 
@@ -74,7 +74,13 @@ bool loadMedia() {
     //Loading success flag
     bool success = true;
 
-	// nothing to load
+    //Load texture
+    gTexture = loadTexture("img/viewport.png");
+    if (gTexture == NULL) {
+        printf("Failed to load texture image!\n");
+        success = false;
+    }
+
     return success;
 }
 
@@ -84,21 +90,21 @@ void close() {
     gTexture = NULL;
 
     //Destroy window
-	SDL_DestroyRenderer(gRenderer);
+    SDL_DestroyRenderer(gRenderer);
     SDL_DestroyWindow(gWindow);
     gRenderer = NULL;
-	gWindow = NULL;
+    gWindow = NULL;
 
     //Quit SDL subsystems
     IMG_Quit();
     SDL_Quit();
 }
 
-SDL_Texture* loadTexture(std::string path){
-	// the final texture
-	SDL_Texture* newTexture = NULL;
+SDL_Texture* loadTexture(std::string path) {
+    // the final texture
+    SDL_Texture* newTexture = NULL;
 
-	//Load image at specified path
+    //Load image at specified path
     SDL_Surface* loadedSurface = IMG_Load(path.c_str());
     if (loadedSurface == NULL) {
         printf("Unable to load image %s! SDL_image Error: %s\n", path.c_str(), IMG_GetError());
@@ -113,7 +119,7 @@ SDL_Texture* loadTexture(std::string path){
         SDL_FreeSurface(loadedSurface);
     }
 
-	return newTexture;
+    return newTexture;
 }
 
 int main(int argc, char* args[]) {
@@ -140,32 +146,45 @@ int main(int argc, char* args[]) {
                         quit = true;
                     }
                 }
-				// clear screen
-				SDL_SetRenderDrawColor(gRenderer, 0xff, 0xff, 0xff, 0xff);
-				SDL_RenderClear(gRenderer);
+                // clear screen
+                SDL_SetRenderDrawColor(gRenderer, 0xff, 0xff, 0xff, 0xff);
+                SDL_RenderClear(gRenderer);
 
-				// render red filled quad
-				SDL_Rect fillRect = { SCREEN_WIDTH / 4, SCREEN_HEIGHT / 4, SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2};
-				SDL_SetRenderDrawColor(gRenderer, 0xff, 0x00, 0x00, 0xff);
-				SDL_RenderFillRect(gRenderer, &fillRect);
+                //Top left corner viewport
+                SDL_Rect topLeftViewport;
+                topLeftViewport.x = 0;
+                topLeftViewport.y = 0;
+                topLeftViewport.w = SCREEN_WIDTH / 2;
+                topLeftViewport.h = SCREEN_HEIGHT / 2;
+                SDL_RenderSetViewport(gRenderer, &topLeftViewport);
 
-				// render green outlined quad
-				SDL_Rect outlineRect = {SCREEN_WIDTH / 6, SCREEN_HEIGHT / 6, SCREEN_WIDTH * 2 / 3, SCREEN_HEIGHT * 2 / 3};
-				SDL_SetRenderDrawColor(gRenderer, 0x00, 0xff, 0x00, 0xff);
-				SDL_RenderDrawRect(gRenderer, &outlineRect);
+                //Render texture to screen
+                SDL_RenderCopy(gRenderer, gTexture, NULL, NULL);
 
-				// draw blue horizontal line
-				SDL_SetRenderDrawColor(gRenderer, 0x00, 0x00, 0xff, 0xff);
-				SDL_RenderDrawLine(gRenderer, 0, SCREEN_HEIGHT / 2, SCREEN_WIDTH, SCREEN_HEIGHT / 2);
+                // top right viewport
+                SDL_Rect topRightViewport;
+                topRightViewport.x = SCREEN_WIDTH / 2;
+                topRightViewport.y = 0;
+                topRightViewport.w = SCREEN_WIDTH / 2;
+                topRightViewport.h = SCREEN_HEIGHT / 2;
+                SDL_RenderSetViewport(gRenderer, &topRightViewport);
 
-				// draw vertical line of yellow dots
-				SDL_SetRenderDrawColor(gRenderer, 0xff, 0xff, 0x00, 0xff);
-				for(int i = 0; i < SCREEN_HEIGHT; i+=4){
-					SDL_RenderDrawPoint(gRenderer, SCREEN_WIDTH / 2, i);
-				}
+                // render texture to screen
+                SDL_RenderCopy(gRenderer, gTexture, NULL, NULL);
 
-				// update screen
-				SDL_RenderPresent(gRenderer);
+                // bottom viewport
+                SDL_Rect bottomViewport;
+                bottomViewport.x = 0;
+                bottomViewport.y = SCREEN_HEIGHT / 2;
+                bottomViewport.w = SCREEN_WIDTH;
+                bottomViewport.h = SCREEN_HEIGHT / 2;
+                SDL_RenderSetViewport(gRenderer, &bottomViewport);
+
+                // render texture to screen
+                SDL_RenderCopy(gRenderer, gTexture, NULL, NULL);
+
+                // update screen
+                SDL_RenderPresent(gRenderer);
             }
         }
     }
