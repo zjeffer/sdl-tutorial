a8fff14f1e5224de2ac99933ec3b23ccaed76eee
zjeffer <vanhouttetuur@gmail.com>
2020-12-21 15:14:13 +0100

    lesson 2: image

--- tutorial.cpp
+++ tutorial.cpp
@@ -6,6 +6,8 @@ and may not be redistributed without written permission.*/
 #include <SDL2/SDL.h>
 #include <stdio.h>
 
+#include <string>
+
 //Screen dimension constants
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
@@ -18,65 +20,80 @@ void toggleFullscreen(SDL_Window* window) {
     SDL_ShowCursor(isFullscreen);
 }
 
+//The window we'll be rendering to
+SDL_Window* gWindow = NULL;
 
-int main( int argc, char* args[] )
-{
-	//The window we'll be rendering to
-	SDL_Window* window = NULL;
-	
-	//The surface contained by the window
-	SDL_Surface* screenSurface = NULL;
+//The surface contained by the window
+SDL_Surface* gScreenSurface = NULL;
 
-	//Initialize SDL
-	if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
-	{
-		printf( "SDL could not initialize! SDL_Error: %s\n", SDL_GetError() );
-	}
-	else
-	{
-		//Create window
-		window = SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
-		if( window == NULL )
-		{
-			printf( "Window could not be created! SDL_Error: %s\n", SDL_GetError() );
-		}
-		else
-		{
-			//Get window surface
-			screenSurface = SDL_GetWindowSurface( window );
+//The image we will load and show on the screen
+SDL_Surface* gHelloWorld = NULL;
 
-			//Fill the surface white
-			SDL_FillRect( screenSurface, NULL, SDL_MapRGB( screenSurface->format, 0xFF, 0xFF, 0xFF ) );
-			
-			//Update the surface
-			SDL_UpdateWindowSurface( window );
+bool init() {
+    // initialization flag
+    bool success = true;
 
-			SDL_SetWindowFullscreen(window, SDL_WINDOW_FULLSCREEN);
-
-			// toggle fullscreen using f
-			// quit program using q
-			bool running = true;
-			for(SDL_Event event; running;){
-				SDL_WaitEvent(&event);
-				if(event.type == SDL_KEYDOWN){
-					switch(event.key.keysym.sym){
-						case SDLK_f: toggleFullscreen(window); break;
-						case SDLK_q: running = false; break;
-					}
-				}
-			}
-
-			//Wait two seconds
-			//SDL_Delay(2000);
-		}
-	}
-
-	//Destroy window
-	SDL_DestroyWindow(window);
-
-	//Quit SDL subsystems
-	SDL_Quit();
-
-	return 0;
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+        success = false;
+    } else {
+        // create window
+        gWindow = SDL_CreateWindow("SDL Tutorial p2", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+        if (gWindow == NULL) {
+            printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
+            success = false;
+        } else {
+            // get window surface
+            gScreenSurface = SDL_GetWindowSurface(gWindow);
+        }
+    }
+    return success;
 }
 
+bool loadMedia() {
+    // loading success flag
+    bool success = true;
+
+    // load splash image
+    const char* imagePath = "img/hello_world.bmp";
+    gHelloWorld = SDL_LoadBMP(imagePath);
+    if (gHelloWorld == NULL) {
+        printf("Unable to load image %s SDL_Error: %s\n", imagePath, SDL_GetError());
+        success = false;
+    }
+    return success;
+}
+
+void closeWindow() {
+    // deallocate surface
+    SDL_FreeSurface(gHelloWorld);
+    gHelloWorld = NULL;
+
+    // destroy window
+    SDL_DestroyWindow(gWindow);
+    gWindow = NULL;
+
+    // quit sdl subsystems
+    SDL_Quit();
+}
+
+int main(int argc, char* args[]) {
+    if (!init()) {
+        printf("Failed to initialize!\n");
+    } else {
+        // load media
+        if (!loadMedia()) {
+            printf("Failed to load media!\n");
+        } else {
+            // apply the loaded surface (gHelloWorld) onto the screen surface
+            SDL_BlitSurface(gHelloWorld, NULL, gScreenSurface, NULL);
+            SDL_UpdateWindowSurface(gWindow);
+            // wait two seconds
+            SDL_Delay(2000);
+        }
+    }
+    // free resources and close sdl
+    closeWindow();
+
+    return 0;
+}
